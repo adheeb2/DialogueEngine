af8c99acd86d7f8a26453a5ff5b52ba8ba87c8d4
adheeb2 <adheebanvar2@gmail.com>
2025-08-21 17:05:46 +0530

    feat(player):added initital backbone of a player

--- dialogue_manager.py
+++ dialogue_manager.py
@@ -1,12 +1,7 @@
-import json
-# When s is used in the ends of load or dump, we know it consist of string either in input or output part(converting to string or reverse)
-# when no s is used, it is for converting to file or reverse
-data = {"name":"adheeb",
-"age":12,
-"foods":["apple","banana","orange"]}
+class Player:
+    def __init__(self,name):
+        self.name = name
+        self.inventory = []
 
-json_string = json.dumps(data,indent=2)#dumps convert dict into json string, indent is just used for readability    
-print(json_string)
-person = json.loads(json_string)#loads JSON string into a Python object(dict/list)
-print(person["foods"][0])
-
+hero = Player("adheeb")
+print(hero.name)
--- player.py
+++ player.py
@@ -1,14 +1,15 @@
 class Player:
-    def __init__(self):
+    def __init__(self,name):
+        self.name = name
         self.inventory = []
         self.quests = {}
-        self.flags = {}
-    def add_item(self, item):
+        self.flags= {}
+    def add_item(self,item):
         self.inventory.append(item)
-        print(f"[You received:{item}]")
+        print(f"[You receieved:{item}] ")
     def remove_item(self,item):
         self.inventory.remove(item)
-        print(f"[You lost:{item}]")
-    def give_quest(self,quest_name):
-        self.quests[quest_name]= "active"
+        print(f"[You lost: {item}]")
+    def give_quests(self,quest_name):
+        self.quests[quest_name] = "active"
         print(f"[New quest:{quest_name}]")
